70523b288a5c09107e18c7b8b53ae79a5428bc3c
Wilfredo Sanchez <wsanchez@apple.com>
2006-08-04 18:26:28 +0000

    Change license to Apache 2.0
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@18 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/method/schedule_common.py
+++ twistedcaldav/method/schedule_common.py
@@ -1,19 +1,16 @@
 ##
 # Copyright (c) 2006 Apple Computer, Inc. All rights reserved.
 #
-# This file contains Original Code and/or Modifications of Original Code
-# as defined in and that are subject to the Apple Public Source License
-# Version 2.0 (the 'License'). You may not use this file except in
-# compliance with the License. Please obtain a copy of the License at
-# http://www.opensource.apple.com/apsl/ and read it before using this
-# file.
-# 
-# The Original Code and all software distributed under the License are
-# distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
-# EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
-# INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
-# Please see the License for the specific language governing rights and
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 #
 # DRI: Cyrus Daboo, cdaboo@apple.com
