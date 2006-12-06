92715da0fb8b91cf74a935c0f45bd92870a16a56
David Reid <dreid@apple.com>
2006-12-06 22:25:59 +0000

    merge backup stuff, only installed on darwin, only used on leopard, only covers restoring the whole document tree from a backup
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@735 e27351fd-9f3e-4f54-a53b-843176b1656c

--- setup.py
+++ setup.py
@@ -73,6 +73,12 @@ version_file.close()
 
 from distutils.core import setup
 
+data_files = [("caldavd", ["conf/repository.xml", "conf/caldavd.plist"])]
+
+if sys.platform == 'darwin':
+    data_files.append(('sbs_backup', ['conf/85-calendar.plist']))
+    data_files.append(('/usr/libexec/sbs_backup', ['bin/calendar_restore']))
+
 setup(
     name             = "twistedcaldav",
     version          = version,
@@ -86,5 +92,5 @@ setup(
     platforms        = [ "all" ],
     packages         = [ "twistedcaldav", "twistedcaldav.directory", "twistedcaldav.method", "twistedcaldav.query" ],
     scripts          = [ "bin/caldavd" ],
-    data_files       = [("caldavd", ["conf/repository.xml", "conf/caldavd.plist"]),],
+    data_files       = data_files
 )
