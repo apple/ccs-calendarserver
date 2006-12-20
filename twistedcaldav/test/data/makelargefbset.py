1bd3676aad2d9659e97de5f37860ac9010097c0f
Cyrus Daboo <cdaboo@apple.com>
2006-12-20 00:40:41 +0000

    Script to setup calendar-free-busy-set property on a bunch of test accounts.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/cdaboo/cuproxy-857@861 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ twistedcaldav/test/data/makelargefbset.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+##
+# Copyright (c) 2005-2006 Apple Computer, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# DRI: Cyrus Daboo, cdaboo@apple.com
+##
+
+import os
+import xattr
+
+user_max = 99
+calendars = ("calendar.1000",)
+
+for ctr in xrange(1, user_max + 1): 
+    path = "calendars/user/user%02d" % (ctr,)
+
+    try: os.makedirs(path)
+    except OSError: pass
+
+    try: os.makedirs(os.path.join(path, "calendar"))
+    except OSError: pass
+
+    for calendar in calendars:
+        inboxname = os.path.join(path, "inbox")
+        attrs = xattr.xattr(inboxname)
+        attrs["WebDAV:{urn:ietf:params:xml:ns:caldav}calendar-free-busy-set"] = """<?xml version='1.0' encoding='UTF-8'?>
+<calendar-free-busy-set xmlns='urn:ietf:params:xml:ns:caldav'>
+  <href xmlns='DAV:'>/calendars/user/user%02d/calendar/</href>
+  <href xmlns='DAV:'>/calendars/user/user%02d/calendar.1000/</href>
+</calendar-free-busy-set>""" % (ctr, ctr,)
