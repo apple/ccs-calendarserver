44911b4477f1e2fc88111408fa5a2f2dcfaf8ba4
David Reid <dreid@apple.com>
2007-02-26 17:29:17 +0000

    Rework the Config object to support reloading, as well as updating the defaults. Add test cases for same.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@1270 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/test/test_config.py
+++ twistedcaldav/test/test_config.py
@@ -18,6 +18,8 @@
 
 from twisted.trial import unittest
 
+from twistedcaldav.py.plistlib import writePlist
+
 from twistedcaldav.config import config, defaultConfig, parseConfig
 
 testConfig = """<?xml version="1.0" encoding="UTF-8"?>
@@ -26,6 +28,8 @@ testConfig = """<?xml version="1.0" encoding="UTF-8"?>
 <dict>
   <key>Verbose</key>
   <true/>
+  <key>HTTPPort</key>
+  <integer>8008</integer>
 </dict>
 </plist>
 """
@@ -38,8 +42,7 @@ class ConfigTests(unittest.TestCase):
 
     def testDefaults(self):
         for key, value in defaultConfig.iteritems():
-            self.failUnless(key in config.__dict__)
-            self.assertEquals(config.__dict__[key], value)
+            self.assertEquals(getattr(config, key), value)
 
     def testParseConfig(self):
         self.assertEquals(config.Verbose, False)
@@ -47,7 +50,7 @@ class ConfigTests(unittest.TestCase):
         parseConfig(self.testConfig)
 
         self.assertEquals(config.Verbose, True)
-    
+
     def testScoping(self):
         def getVerbose():
             self.assertEquals(config.Verbose, True)
@@ -59,3 +62,53 @@ class ConfigTests(unittest.TestCase):
         self.assertEquals(config.Verbose, True)
 
         getVerbose()
+
+    def testReloading(self):
+        self.assertEquals(config.HTTPPort, None)
+
+        parseConfig(self.testConfig)
+
+        self.assertEquals(config.HTTPPort, 8008)
+
+        writePlist({}, self.testConfig)
+
+        config.reload()
+
+        self.assertEquals(config.HTTPPort, None)
+
+    def testUpdateAndReload(self):
+        self.assertEquals(config.HTTPPort, None)
+
+        parseConfig(self.testConfig)
+
+        self.assertEquals(config.HTTPPort, 8008)
+
+        config.update({'HTTPPort': 80})
+
+        self.assertEquals(config.HTTPPort, 80)
+
+        config.reload()
+
+        self.assertEquals(config.HTTPPort, 8008)
+
+    def testUpdating(self):
+        self.assertEquals(config.SSLPort, None)
+
+        config.update({'SSLPort': 8443})
+
+        self.assertEquals(config.SSLPort, 8443)
+
+    def testUpdateDefaults(self):
+        self.assertEquals(config.SSLPort, None)
+
+        parseConfig(self.testConfig)
+
+        config.updateDefaults({'SSLPort': 8009})
+
+        self.assertEquals(config.SSLPort, 8009)
+
+        config.reload()
+
+        self.assertEquals(config.SSLPort, 8009)
+
+        config.updateDefaults({'SSLPort': None})
