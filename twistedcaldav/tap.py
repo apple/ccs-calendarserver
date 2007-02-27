1591e08339fdb5993b3751e58ab687a86c2f1dc2
David Reid <dreid@apple.com>
2007-02-27 20:30:34 +0000

    Add admin principal for caldavd-test.plist
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@1278 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/tap.py
+++ twistedcaldav/tap.py
@@ -147,7 +147,11 @@ class CalDAVOptions(Options):
                 raise UsageError("Only root can drop privileges, you are: %s"
                                  % (groupname,))
 
-        self.parent['logfile'] = config.ErrorLogFile
+        if self.parent['nodaemon']:
+            self.parent['logfile'] = None
+        else:
+            self.parent['logfile'] = config.ErrorLogFile
+
         self.parent['pidfile'] = config.PIDFile
 
         # Verify that document root actually exists
