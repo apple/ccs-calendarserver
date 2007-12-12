401a3db6f00b388bd4deb49a82911d12b0a575b6
Wilfredo Sanchez <wsanchez@apple.com>
2007-12-12 00:58:32 +0000

    Get rid of DRI hooey
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@2065 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/test/util.py
+++ twistedcaldav/test/util.py
@@ -12,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# DRI: Wilfredo Sanchez, wsanchez@apple.com
 ##
 
 import twisted.web2.dav.test.util
