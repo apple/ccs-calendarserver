bc963e429701890fe7401be136583ce3d742e85c
Cyrus Daboo <cdaboo@apple.com>
2008-10-22 03:34:24 +0000

    Remove option to manually trigger auto-accept behavior. The way to do that now is to simply re-invite the auto-accept room/resource as the organizer using PUT.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@3206 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/config.py
+++ twistedcaldav/config.py
@@ -187,7 +187,6 @@ defaultConfig = {
     "EnableDropBox"           : False, # Calendar Drop Box
     "EnablePrivateEvents"     : False, # Private Events
     "EnableTimezoneService"   : False, # Timezone service
-    "EnableAutoAcceptTrigger" : False, # Manually trigger auto-accept behavior
     
     #
     # Scheduling related options
