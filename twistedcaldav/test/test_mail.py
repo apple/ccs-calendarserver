c7b8edcce845f80cf796ec5d0328af99525ee390
Cyrus Daboo <cdaboo@apple.com>
2008-11-13 21:19:35 +0000

    Don't test for icons if the main path is not present.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@3379 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/test/test_mail.py
+++ twistedcaldav/test/test_mail.py
@@ -33,10 +33,11 @@ class MailHandlerTests(TestCase):
     def test_iconPath(self):
         iconPath = self.handler.getIconPath({'day':'1', 'month':'1'}, False, language='en')
         iconDir = "/usr/share/caldavd/share/date_icons"
-        if os.path.exists("%s/JAN/01.png" % (iconDir,)):
-            self.assertEquals(iconPath, "%s/JAN/01.png" % (iconDir,))
-        else:
-            self.assertEquals(iconPath, "%s/01/01.png" % (iconDir,))
+        if os.path.exists(iconDir):
+            if os.path.exists("%s/JAN/01.png" % (iconDir,)):
+                self.assertEquals(iconPath, "%s/JAN/01.png" % (iconDir,))
+            else:
+                self.assertEquals(iconPath, "%s/01/01.png" % (iconDir,))
 
     def test_checkDSNFailure(self):
 
