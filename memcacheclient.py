2ddcbf2c4089a56916ebffc6f7baae6e12793eb5
Cyrus Daboo <cdaboo@apple.com>
2009-04-17 20:41:51 +0000

    Better error timeout handling.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/wsanchez/deployment@4033 e27351fd-9f3e-4f54-a53b-843176b1656c

--- memcacheclient.py
+++ memcacheclient.py
@@ -930,7 +930,7 @@ class Client(local):
 
 
 class _Host:
-    _DEAD_RETRY = 30  # number of seconds before retrying a dead server.
+    _DEAD_RETRY = 1  # number of seconds before retrying a dead server.
     _SOCKET_TIMEOUT = 3  #  number of seconds before sockets timeout.
 
     def __init__(self, host, debugfunc=None):
