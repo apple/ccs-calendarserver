18149590351f8bb47b85f909b1eb53f143826de1
Morgen Sagen <sagen@apple.com>
2009-04-18 00:15:41 +0000

    memcache.py was renamed to memcacheclient.py
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@4035 e27351fd-9f3e-4f54-a53b-843176b1656c

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ dist = setup(
     scripts          = [ "bin/caldavd", "bin/caldav_export" ],
     data_files       = [ ("caldavd", ["conf/caldavd.plist"]) ],
     ext_modules      = extensions,
-    py_modules       = ["kqreactor", "memcache"],
+    py_modules       = ["kqreactor", "memcacheclient"],
 )
 
 if "install" in dist.commands:
