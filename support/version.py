0afbb240fd1cb63dac7c4fdc77a2993f182806c6
Wilfredo Sanchez <wsanchez@apple.com>
2009-04-29 00:14:20 +0000

    trunk is 2.3
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@4104 e27351fd-9f3e-4f54-a53b-843176b1656c

--- support/version.py
+++ support/version.py
@@ -24,7 +24,7 @@ def version():
     # Compute the version number.
     #
 
-    base_version = "2.2"
+    base_version = "2.3"
 
     branches = (
         "tags/release/CalendarServer-" + base_version,
