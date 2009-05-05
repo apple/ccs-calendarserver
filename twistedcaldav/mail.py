874cd4cc8f0452126a50325c03c17615610107a3
Wilfredo Sanchez <wsanchez@apple.com>
2009-05-05 01:12:44 +0000

    Pulled up r4157 r4158 from trunk.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/release/CalendarServer-2.2-dev@4169 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/mail.py
+++ twistedcaldav/mail.py
@@ -39,6 +39,7 @@ from twisted.web2.dav.noneprops import NonePropertyStore
 from twisted.web2.http import Response, HTTPError
 from twisted.web2.http_headers import MimeType
 
+from twistedcaldav.directory.digest import QopDigestCredentialFactory
 from twistedcaldav import ical, caldavxml
 from twistedcaldav.config import config, defaultConfig, defaultConfigFile
 from twistedcaldav.ical import Property
@@ -183,6 +184,7 @@ class IMIPInboxResource(CalDAVFile):
         CalDAVFile.__init__(self, NotFilePath(isfile=True), principalCollections=parent.principalCollections())
 
         self.parent = parent
+
 
     def accessControlList(self, request, inheritance=True,
         expanding=False, inherited_aces=None):
