9e15b7a79f7d8fab934b41b37e1b8ab20931541b
Morgen Sagen <sagen@apple.com>
2010-02-08 05:17:35 +0000

    Missing a comma
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/sagen/locations-resources@5050 e27351fd-9f3e-4f54-a53b-843176b1656c

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ dist = setup(
     scripts          = [
                          "bin/caldavd",
                          "bin/calendarserver_export",
-                         "bin/calendarserver_manage_principals"
+                         "bin/calendarserver_manage_principals",
                          "bin/calendarserver_command_gateway"
                        ],
     data_files       = [ ("caldavd", ["conf/caldavd.plist"]) ],
