5aacda6c7ff0206a3280f6359024f60f2aef6291
Wilfredo Sanchez <wsanchez@apple.com>
2010-03-23 03:18:47 +0000

    Add none property store.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@5381 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ txdav/propertystore/none.py
@@ -0,0 +1,89 @@
+##
+# Copyright (c) 2010 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+Property store with no storage.
+"""
+
+from __future__ import absolute_import
+
+__all__ = [
+    "PropertyStore",
+]
+
+import sys
+import errno
+import urllib
+from zlib import compress, decompress, error as ZlibError
+from cPickle import UnpicklingError, loads as unpickle
+
+from twext.web2.dav.davxml import WebDAVDocument
+
+from txdav.propertystore.base import AbstractPropertyStore, PropertyName
+from txdav.idav import PropertyChangeNotAllowedError
+
+
+class PropertyStore(AbstractPropertyStore):
+    """
+    Property store with no storage.
+    """
+    def __init__(self):
+        self.modified = {}
+
+    def __str__(self):
+        return "<%s>" % (self.__class__.__name__,)
+
+    #
+    # Accessors
+    #
+
+    def __delitem__(self, key):
+        if key in self.modified:
+            del self.modified[key]
+        else:
+            raise KeyError(key)
+
+    def __getitem__(self, key):
+        if key in self.modified:
+            return self.modified[key]
+        else:
+            raise KeyError(key)
+
+    def __contains__(self, key):
+        return key in self.modified
+
+    def __setitem__(self, key, value):
+        self.modified[key] = value
+
+    def __iter__(self):
+        return (k for k in self.modified)
+
+    def __len__(self):
+        return len(self.modified)
+
+    #
+    # I/O
+    #
+
+    def flush(self):
+        if self.modified:
+            raise PropertyChangeNotAllowedError(
+                "None property store cannot flush changes.",
+                keys = self.modified.keys()
+            )
+
+    def abort(self):
+        self.modified.clear()
---
+++ txdav/propertystore/test/test_none.py
@@ -0,0 +1,86 @@
+##
+# Copyright (c) 2010 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+Property store tests.
+"""
+
+from zope.interface.verify import verifyObject, BrokenMethodImplementation
+
+from twisted.trial import unittest
+
+from twext.python.filepath import FilePath
+from twext.web2.dav import davxml
+
+from txdav.idav import IPropertyStore, PropertyChangeNotAllowedError
+from txdav.propertystore.base import PropertyName
+from txdav.propertystore.none import PropertyStore
+
+
+class PropertyStoreTest(unittest.TestCase):
+    def setUp(self):
+        self.propertyStore = PropertyStore()
+
+    def test_interface(self):
+        try:
+            verifyObject(IPropertyStore, self.propertyStore)
+        except BrokenMethodImplementation, e:
+            self.fail(e)
+
+    def test_flush(self):
+        store = self.propertyStore
+
+        # Flushing no changes is ok
+        store.flush()
+
+        name = propertyName("test")
+        value = davxml.ResponseDescription("Hello, World!")
+
+        store[name] = value
+
+        # Flushing changes isn't allowed
+        self.assertRaises(PropertyChangeNotAllowedError, store.flush)
+
+        # Changes are still here
+        self.assertEquals(store.get(name, None), value)
+
+        # Flushing no changes is ok
+        del store[name]
+        store.flush()
+
+        self.assertEquals(store.get(name, None), None)
+
+
+    def test_abort(self):
+        store = self.propertyStore
+
+        name = propertyName("test")
+        value = davxml.ResponseDescription("Hello, World!")
+
+        store[name] = value
+
+        store.abort()
+
+        self.assertEquals(store.get(name, None), None)
+        self.assertEquals(store.modified, {})
+
+
+if PropertyStore is None:
+    PropertyStoreTest.skip = importErrorMessage
+
+
+def propertyName(name):
+    return PropertyName("http://calendarserver.org/ns/test/", name)
