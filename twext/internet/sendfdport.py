b65574d1774d3331cd9339c17784d9b280f79204
Glyph Lefkowitz <glyph@apple.com>
2010-07-07 18:02:45 +0000

    pull up r5853 from deployment
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@5855 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twext/internet/sendfdport.py
+++ twext/internet/sendfdport.py
@@ -274,7 +274,10 @@ class InheritedPort(FileDescriptor, object):
                 # should this be on the transportFactory's side of things?
 
                 close(fd)       # fromfd() calls dup()
-                peeraddr = skt.getpeername()
+                try:
+                    peeraddr = skt.getpeername()
+                except SocketError:
+                    peeraddr = ('0.0.0.0', 0)
                 protocol = self.protocolFactory.buildProtocol(peeraddr)
                 transport = self.transportFactory(skt, description, protocol)
                 protocol.makeConnection(transport)
