e8551f0011a7f2ba53800b23bb3e64caadf2cbc3
Wilfredo Sanchez <wsanchez@apple.com>
2010-07-27 18:40:13 +0000

    Add TIME_RANGE, misc cleanup
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@5953 e27351fd-9f3e-4f54-a53b-843176b1656c

--- txcaldav/calendarstore/postgres_schema_v1.sql
+++ txcaldav/calendarstore/postgres_schema_v1.sql
@@ -15,6 +15,36 @@ create table CALENDAR_HOME (
 );
 
 
+--------------
+-- Calendar --
+--------------
+
+create table CALENDAR (
+  RESOURCE_ID varchar(255) primary key default nextval('RESOURCE_ID_SEQ'),
+  SYNC_TOKEN  varchar(255)
+);
+
+
+-------------------
+-- Calendar Bind --
+-------------------
+
+-- Joins CALENDAR_HOME and CALENDAR
+
+create table CALENDAR_BIND (
+  CALENDAR_HOME_RESOURCE_ID varchar(255) not null references CALENDAR_HOME,
+  CALENDAR_RESOURCE_ID      varchar(255) not null references CALENDAR,
+  CALENDAR_RESOURCE_NAME    varchar(255) not null,
+  BIND_MODE                 integer      not null, -- enum CALENDAR_BIND_MODE
+  BIND_STATUS               integer      not null, -- enum CALENDAR_BIND_STATUS
+  SEEN_BY_OWNER             bool         not null,
+  SEEN_BY_SHAREE            bool         not null,
+  MESSAGE                   text,                  -- FIXME: xml?
+
+  primary key(CALENDAR_HOME_RESOURCE_ID, CALENDAR_RESOURCE_ID),
+  unique(CALENDAR_HOME_RESOURCE_ID, CALENDAR_RESOURCE_NAME)
+);
+
 -- Enumeration of calendar bind modes
 
 create table CALENDAR_BIND_MODE (
@@ -38,36 +68,6 @@ insert into CALENDAR_BIND_STATUS values (1, 'accepted');
 insert into CALENDAR_BIND_STATUS values (2, 'declined');
 
 
---------------
--- Calendar --
---------------
-
-create table CALENDAR (
-  RESOURCE_ID varchar(255) primary key default nextval('RESOURCE_ID_SEQ'),
-  SYNC_TOKEN  varchar(255)
-);
-
-
--------------------
--- Calendar Bind --
--------------------
-
--- Joins CALENDAR_HOME and CALENDAR
-
-create table CALENDAR_BIND (
-  CALENDAR_HOME_RESOURCE_ID varchar(255) not null references CALENDAR_HOME,
-  CALENDAR_RESOURCE_ID      varchar(255) not null references CALENDAR,
-  CALENDAR_RESOURCE_NAME    varchar(255) not null,
-  CALENDAR_MODE             int          not null,
-  SEEN_BY_OWNER             bool         not null,
-  SEEN_BY_SHAREE            bool         not null,
-  STATUS                    integer      not null,
-  MESSAGE                   text,                  -- FIXME: xml?
-
-  primary key(CALENDAR_HOME_RESOURCE_ID, CALENDAR_RESOURCE_ID),
-  unique(CALENDAR_HOME_RESOURCE_ID, CALENDAR_RESOURCE_NAME)
-);
-
 ---------------------
 -- Calendar Object --
 ---------------------
@@ -79,7 +79,7 @@ create table CALENDAR_OBJECT (
   ICALENDAR_TEXT       text         not null,
   ICALENDAR_UID        varchar(255) not null,
   ICALENDAR_TYPE       varchar(255) not null,
-  ATTACHMENTS_MODE     int          not null,
+  ATTACHMENTS_MODE     int          not null, -- enum CALENDAR_OBJECT_ATTACHMENTS_MODE
   ORGANIZER            varchar(255),
   ORGANIZER_OBJECT     varchar(255) references CALENDAR_OBJECT,
 
@@ -99,16 +99,44 @@ insert into CALENDAR_OBJECT_ATTACHMENTS_MODE values (1, 'write');
 
 
 ----------------
+-- Time Range --
+----------------
+
+create table TIME_RANGE (
+  CALENDAR_OBJECT_RESOURCE_ID varchar(255) not null references CALENDAR_OBJECT,
+  FLOATING                    bool         not null,
+  START_DATE                  date         not null,
+  END_DATE                    date         not null,
+  FBTYPE                      integer      not null, -- enum FREE_BUSY_TYPE
+  TRANSPARENT                 bool         not null
+);
+
+-- Enumeration of free/busy types
+
+create table FREE_BUSY_TYPE (
+  ID          int         primary key,
+  DESCRIPTION varchar(16) not null unique
+);
+
+insert into FREE_BUSY_TYPE values (0, 'unknown'         );
+insert into FREE_BUSY_TYPE values (1, 'free'            );
+insert into FREE_BUSY_TYPE values (2, 'busy'            );
+insert into FREE_BUSY_TYPE values (3, 'busy-unavailable');
+insert into FREE_BUSY_TYPE values (4, 'busy-tentative'  );
+
+
+----------------
 -- Attachment --
 ----------------
 
 create table ATTACHMENT (
-  CALENDAR_OBJECT varchar(255) not null references CALENDAR_OBJECT,
-  CONTENT_TYPE    varchar(255) not null,
-  SIZE            int          not null,
-  MD5             char(32)     not null,
-  PATH            varchar(255) not null unique
+  CALENDAR_OBJECT_RESOURCE_ID varchar(255) not null references CALENDAR_OBJECT,
+  CONTENT_TYPE                varchar(255) not null,
+  SIZE                        int          not null,
+  MD5                         char(32)     not null,
+  PATH                        varchar(255) not null unique
 );
+
 
 ------------------
 -- iTIP Message --
