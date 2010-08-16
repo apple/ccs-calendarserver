fa1d1187d37b0926ac8ec44a74aac5e4efe7421d
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2010-08-16 21:56:34 +0000

    Remove hard-coded PIDs; replace with command line arguments
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@6085 e27351fd-9f3e-4f54-a53b-843176b1656c

--- contrib/performance/benchmark.sh
+++ contrib/performance/benchmark.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo PYTHONPATH=$PYTHONPATH:../../../vobject/:../../../../CalDAVClientLibrary/trunk/src:../../../Twisted/ python mkcal.py 5466 5465
+sudo PYTHONPATH=$PYTHONPATH:../../../vobject/:../../../../CalDAVClientLibrary/trunk/src:../../../Twisted/ python -i mkcal.py "$@"
+
