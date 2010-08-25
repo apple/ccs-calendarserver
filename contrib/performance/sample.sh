ac3cb08f4146615549c6b292273acfe01801fdb6
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2010-08-25 20:45:09 +0000

    a script for running benchmarks against multiple backends and uploading the results
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@6183 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ contrib/performance/sample.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+set -e
+
+BACKENDS="filesystem postgresql"
+
+SOURCE=~/Projects/CalendarServer/trunk
+BENCHMARKS="vfreebusy event"
+ADDURL=http://localhost:8000/result/add/
+export PYTHONPATH=$PYTHONPATH:$SOURCE/../Twisted
+
+REV=$1
+
+pushd $SOURCE
+svn up -r$REV .
+python setup.py build_ext -i
+popd
+
+for backend in $BACKENDS; do
+  ./setbackend $SOURCE/conf/caldavd-test.plist $backend > $SOURCE/conf/caldavd-dev.plist
+  pushd $SOURCE
+  ./run -k || true
+  rm -rf data/
+  ./run -d -n
+  popd
+  sleep 5
+  ./benchmark --label r$REV-$backend $BENCHMARKS
+  data=`echo -n r$REV*`
+  for p in 1 9 81; do
+    for b in $BENCHMARKS; do
+      ./upload \
+          --url $ADDURL --revision $REV \
+          --revision-date "`./svn-committime $SOURCE`" --environment nmosbuilder \
+          --backend $backend --statistic "$data,$b,$p,urlopen time"
+    done
+  done
+
+  mv $data old-results/
+done
