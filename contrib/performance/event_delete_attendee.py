3d2dc137775a9e6db2d1619f1de163a64ea36263
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2010-09-14 17:53:54 +0000

    Benchmark removal of attendees from events
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@6288 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ contrib/performance/event_delete_attendee.py
@@ -0,0 +1,20 @@
+
+import _event_change
+
+def measure(host, port, dtrace, attendeeCount, samples):
+    def deleteAttendees(event, i):
+        """
+        Add C{i} new attendees to the given event.
+        """
+        for n in range(attendeeCount):
+            # Find the beginning of an ATTENDEE line
+            attendee = event.find('ATTENDEE')
+            # And the end of it
+            eol = event.find('\n', attendee)
+            # And remove it
+            event = event[:attendee] + event[eol:]
+        return event
+
+    return _event_change.measure(
+        host, port, dtrace, attendeeCount, samples, "date", deleteAttendees,
+        eventPerSample=True)
