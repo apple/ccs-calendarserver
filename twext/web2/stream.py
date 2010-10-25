c7d31daf15feb3b738157696e9e3f2883db4df6e
Cyrus Daboo <cdaboo@apple.com>
2010-10-25 18:48:18 +0000

    Undo some debugging code not yet in svn.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@6465 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twext/web2/stream.py
+++ twext/web2/stream.py
@@ -654,7 +654,7 @@ class ProducerStream(object):
                 return defer.fail(f)
             return None
         else:
-            deferred = self.deferred = Deferred(noInlineCallbackDebugging=True)
+            deferred = self.deferred = Deferred()
             if self.producer is not None and (not self.streamingProducer
                                               or self.producerPaused):
                 self.producerPaused = False
