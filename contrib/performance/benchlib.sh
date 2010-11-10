f580060ec697df05ceb3a704464dddbe7ca15f62
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2010-11-10 00:24:17 +0000

    Some comments about what's in this file; also fix the upload hostname to refer to localhost again.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@6583 e27351fd-9f3e-4f54-a53b-843176b1656c

--- contrib/performance/benchlib.sh
+++ contrib/performance/benchlib.sh
@@ -2,17 +2,30 @@
 set -e # Break on error
 shopt -s nullglob # Expand foo* to nothing if nothing matches
 
+# Names of database backends that can be benchmarked.
 BACKENDS="filesystem postgresql"
 
+# Location of the CalendarServer source.  Will automatically be
+# updated to the appropriate version, config edited to use the right
+# backend, and PID files will be discovered beneath it.
 SOURCE=~/Projects/CalendarServer/trunk
-BENCHMARKS="event_move event_delete_attendee event_add_attendee event_change_date event_change_summary event_delete vfreebusy event"
-STATISTICS=(HTTP SQL read write pagein pageout)
-ADDURL=http://boson.:8000/result/add/
 
+# Names of benchmarks we can run.
+BENCHMARKS="event_move event_delete_attendee event_add_attendee event_change_date event_change_summary event_delete vfreebusy event"
+
+# Names of metrics we can collect.
+STATISTICS=(HTTP SQL read write pagein pageout)
+
+# Codespeed add-result location.
+ADDURL=http://localhost:8000/result/add/
+
+# Change the config beneath $SOURCE to use a particular database backend.
 function setbackend() {
   ./setbackend $SOURCE/conf/caldavd-test.plist $1 > $SOURCE/conf/caldavd-dev.plist
 }
 
+# Clean up $SOURCE, update to the specified revision, and build the
+# extensions (in-place).
 function update_and_build() {
   pushd $SOURCE
   stop
@@ -22,6 +35,9 @@ function update_and_build() {
   popd
 }
 
+# Start a CalendarServer in the current directory.  Only return after
+# the specified number of slave processes have written their PID files
+# (which is only a weak metric for "the server is ready to use").
 function start() {
   NUM_INSTANCES=$1
   shift
@@ -36,6 +52,8 @@ function start() {
   done
 }
 
+# Stop the CalendarServer in the current directory.  Only return after
+# it has exited.
 function stop() {
   ./run -k || true
   while [ -e ./data/Logs/caldavd.pid ]; do
