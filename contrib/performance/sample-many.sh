6cdeed0b1c73ea0911b650cf8f092a27ed0f13be
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2010-11-18 19:29:53 +0000

    Add copyright boilerplate
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@6651 e27351fd-9f3e-4f54-a53b-843176b1656c

--- contrib/performance/sample-many.sh
+++ contrib/performance/sample-many.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+##
+# Copyright (c) 2010 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
 for i in `python -c "for i in range($START, $STOP, $STEP): print i,"`; do
     ./sample.sh $i ~/Projects/CalendarServer/trunk/data/Logs "$1"
 done
