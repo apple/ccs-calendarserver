338370ec5d84b5000c1bb50d344b8ae940059020
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2010-12-02 22:05:49 +0000

    sample.sh no longer takes a log directory, just a source checkout root directory
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@6667 e27351fd-9f3e-4f54-a53b-843176b1656c

--- contrib/performance/sample-many.sh
+++ contrib/performance/sample-many.sh
@@ -17,5 +17,5 @@
 ##
 
 for i in `python -c "for i in range($START, $STOP, $STEP): print i,"`; do
-    ./sample.sh $i ~/Projects/CalendarServer/trunk/data/Logs "$1"
+    ./sample.sh $i ~/Projects/CalendarServer/trunk/ "$1"
 done
