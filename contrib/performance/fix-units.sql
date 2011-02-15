1d6af82c5aad1758c93c0457ede546adb0b1c17f
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2011-02-15 15:43:03 +0000

    Give paging metrics a different unit to slightly improve the graphs on the comparison page.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@6927 e27351fd-9f3e-4f54-a53b-843176b1656c

--- contrib/performance/fix-units.sql
+++ contrib/performance/fix-units.sql
@@ -19,11 +19,11 @@
 update codespeed_benchmark set units_title = 'Statements' where name like '%-SQLcount';
 update codespeed_benchmark set units_title = 'Bytes' where name like '%-read';
 update codespeed_benchmark set units_title = 'Bytes' where name like '%-write';
-update codespeed_benchmark set units_title = 'Bytes' where name like '%-pagein';
-update codespeed_benchmark set units_title = 'Bytes' where name like '%-pageout';
+update codespeed_benchmark set units_title = 'Pages' where name like '%-pagein';
+update codespeed_benchmark set units_title = 'Pages' where name like '%-pageout';
 
 update codespeed_benchmark set units = 'statements' where name like '%-SQLcount';
 update codespeed_benchmark set units = 'bytes' where name like '%-read'; 
 update codespeed_benchmark set units = 'bytes' where name like '%-write';
-update codespeed_benchmark set units = 'bytes' where name like '%-pagein';
-update codespeed_benchmark set units = 'bytes' where name like '%-pageout';
+update codespeed_benchmark set units = 'pages' where name like '%-pagein';
+update codespeed_benchmark set units = 'pages' where name like '%-pageout';
