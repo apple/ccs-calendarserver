f085728339e82b49491f6b6ff36ddac53cb62ce6
Glyph Lefkowitz <glyph@apple.com>
2011-05-16 14:42:43 +0000

    Make a note about data that the export tool does not deal with.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/glyph/new-export@7458 e27351fd-9f3e-4f54-a53b-843176b1656c

--- calendarserver/tools/export.py
+++ calendarserver/tools/export.py
@@ -29,6 +29,11 @@ data storage; it does not operate by talking to the server via the
 network.  It therefore does not apply any of the access restrictions
 that the server would.  As such, one should be midful that data
 exported via this tool may be sensitive.
+
+Please also note that this is not an appropriate tool for backups, as there is
+data associated with users and calendars beyond the iCalendar as visible to the
+owner of that calendar, including DAV properties, information about sharing, and
+per-user data such as alarms.
 """
 
 import os
