a523d57a8fa4c8b5a1d21a121125ed7fc44f5964
Wilfredo Sanchez <wsanchez@apple.com>
2011-06-24 22:05:57 +0000

    Shell of a shell
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@7666 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ calendarserver/tools/shell.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- test-case-name: calendarserver.tools.test.test_export -*-
+##
+# Copyright (c) 2011 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+Interactive shell for navigating the data store.
+"""
+
+import os
+import sys
+
+from twisted.python.text import wordWrap
+from twisted.python.usage import Options, UsageError
+from twisted.application.service import Service
+
+from twistedcaldav.stdconfig import DEFAULT_CONFIG_FILE
+
+from calendarserver.tools.cmdline import utilityMain
+
+
+def usage(e=None):
+    if e:
+        print e
+        print ""
+    try:
+        ShellOptions().opt_help()
+    except SystemExit:
+        pass
+    if e:
+        sys.exit(64)
+    else:
+        sys.exit(0)
+
+
+class ShellOptions(Options):
+    """
+    Command line options for "calendarserver_shell".
+    """
+    synopsis = "\n".join(
+        wordWrap(
+            """
+            Usage: calendarserver_shell [options]\n
+            """ + __doc__,
+            int(os.environ.get("COLUMNS", "80"))
+        )
+    )
+
+    optParameters = [
+        ["config", "f", DEFAULT_CONFIG_FILE, "Specify caldavd.plist configuration path."],
+    ]
+
+    def __init__(self):
+        super(ShellOptions, self).__init__()
+
+
+class ShellService(Service):
+    def __init__(self, store, options, reactor, config):
+        super(ShellService, self).__init__()
+        self.store   = store
+        self.options = options
+        self.reactor = reactor
+        self.config = config
+
+    def startService(self):
+        """
+        Start the service.
+        """
+        super(ShellService, self).startService()
+        self.shell()
+
+    def stopService(self):
+        """
+        Stop the service.
+        """
+
+    def shell(self):
+        """
+        Interactive shell
+        """
+        print "This is a shell, yo."
+
+
+def main(argv=sys.argv, stderr=sys.stderr, reactor=None):
+    """
+    Do the export.
+    """
+    if reactor is None:
+        from twisted.internet import reactor
+
+    options = ShellOptions()
+    try:
+        options.parseOptions(argv[1:])
+    except UsageError, e:
+        usage(e)
+
+    def makeService(store):
+        from twistedcaldav.config import config
+        return ShellService(store, options, reactor, config)
+
+    utilityMain(options["config"], makeService, reactor)
