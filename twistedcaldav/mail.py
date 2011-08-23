be4947ac7267723ea91e2fb5d3a9306674c7bf17
Glyph Lefkowitz <glyph@apple.com>
2011-08-23 02:53:33 +0000

    Assign an XHTML/Transitional doctype to the converted document, to allow for the use of HTML entities like &nbsp;.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@8006 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/mail.py
+++ twistedcaldav/mail.py
@@ -191,6 +191,10 @@ def _fixup(data, rendererName):
     document.documentElement.setAttribute(
         "xmlns:t", TEMPLATE_NAMESPACE
     )
+    document.doctype = (
+        'html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" '
+        '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"'
+    )
     document.documentElement.setAttribute(
         "t:render", rendererName
     )
