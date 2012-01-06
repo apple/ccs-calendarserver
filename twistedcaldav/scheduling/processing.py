eba11dec29a6d4f9a23ba3107c1ff238f42b2d2a
Cyrus Daboo <cdaboo@apple.com>
2012-01-06 14:59:26 +0000

    Prevent itip.auto from always appearing on itip refresh access log entries.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@8489 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/scheduling/processing.py
+++ twistedcaldav/scheduling/processing.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2005-2011 Apple Inc. All rights reserved.
+# Copyright (c) 2005-2012 Apple Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -528,7 +528,6 @@ class ImplicitProcessor(object):
                 
                 # Refresh from another Attendee should not have Inbox item
                 if hasattr(self.request, "doing_attendee_refresh"):
-                    autoprocessed = True
                     store_inbox = False
 
                 result = (True, autoprocessed, store_inbox, changes,)
