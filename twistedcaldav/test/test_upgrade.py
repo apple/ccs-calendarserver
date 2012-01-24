5a376c60234bdb0103982ba4739ac9bbdd9aaaa8
Glyph Lefkowitz <glyph@apple.com>
2012-01-24 08:58:08 +0000

    parallel version of the upgrade_to_1 test cases
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/glyph/parallel-upgrade_to_1@8579 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/test/test_upgrade.py
+++ twistedcaldav/test/test_upgrade.py
@@ -1597,3 +1597,15 @@ def isValidCTag(value):
         return True
     except ValueError:
         return False
+
+
+class ParallelUpgradeTests(UpgradeTests):
+    """
+    Tests for upgradeData in parallel.
+    """
+
+    def doUpgrade(self, config):
+        from txdav.common.datastore.upgrade.test.test_migrate import StubSpawner
+        spawner = StubSpawner(config)
+        return upgradeData(config, spawner, 2)
+
