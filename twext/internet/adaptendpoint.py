78a86c544f41e4dada9b9f23e2d020af3aef363c
Glyph Lefkowitz <glyph@apple.com>
2012-04-13 18:45:40 +0000

    whitespace
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/glyph/ipv6-client@9075 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twext/internet/adaptendpoint.py
+++ twext/internet/adaptendpoint.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ##
+
 """
 Adapter for old-style connectTCP/connectSSL code to use endpoints and be happy;
 specifically, to receive the additional duplicate notifications that it wants to
