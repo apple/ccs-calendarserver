2655cdc929aea569f88f4577667b52f20d843581
Glyph Lefkowitz <glyph@apple.com>
2012-04-27 00:08:47 +0000

    skip test_shareWithUID for file and implicit stores
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/glyph/sharing-api@9195 e27351fd-9f3e-4f54-a53b-843176b1656c

--- txdav/caldav/datastore/test/test_file.py
+++ txdav/caldav/datastore/test/test_file.py
@@ -465,6 +465,14 @@ class FileStorageTests(CommonTests, unittest.TestCase):
         return self.calendarStore
 
 
+    def test_shareWithUID(self):
+        """
+        Overridden to be skipped.
+        """
+
+    test_shareWithUID.skip = "Not implemented for file store yet."
+
+
     def test_init(self):
         """
         L{CalendarStore} has a C{_path} attribute which refers to its
