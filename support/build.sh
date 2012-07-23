0686433c6b1176b73f8df1648cbee3dae4aad4b2
Cyrus Daboo <cdaboo@apple.com>
2012-07-23 14:37:35 +0000

    Use latest PyKerberos revision.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@9479 e27351fd-9f3e-4f54-a53b-843176b1656c

--- support/build.sh
+++ support/build.sh
@@ -698,7 +698,7 @@ dependencies () {
     "http://pypi.python.org/packages/source/p/pyOpenSSL/${po}.tar.gz";
 
   if type -P krb5-config > /dev/null; then
-    py_dependency -r 8679 \
+    py_dependency -r 9409 \
       "PyKerberos" "kerberos" "PyKerberos" \
       "${svn_uri_base}/PyKerberos/trunk";
   fi;
