2a026cdab065727968bf62e40406ecdb31fcf3fa
Glyph Lefkowitz <glyph@apple.com>
2013-01-05 00:39:02 +0000

    Transplant locking classes to their own isolated area.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/glyph/queue-locking-and-timing@10245 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twext/enterprise/locking.py
+++ twext/enterprise/locking.py
@@ -18,3 +18,56 @@
 """
 Utilities to restrict concurrency based on mutual exclusion.
 """
+
+from twext.enterprise.dal.model import Table
+from twext.enterprise.dal.model import SQLType
+from twext.enterprise.dal.model import Constraint
+from twext.enterprise.dal.syntax import SchemaSyntax
+from twext.enterprise.dal.model import Schema
+
+
+def makeLockSchema(inSchema):
+    """
+    Create a self-contained schema just for L{Locker} use, in C{inSchema}.
+
+    @param inSchema: a L{Schema} to add the locks table to.
+    @type inSchema: L{Schema}
+
+    @return: inSchema
+    """
+    LockTable = Table(inSchema, 'NAMED_LOCKS')
+
+    LockTable.addColumn("LOCK_NAME", SQLType("varchar", 255))
+    LockTable.tableConstraint(Constraint.NOT_NULL, ["LOCK_NAME"])
+    LockTable.tableConstraint(Constraint.UNIQUE, ["LOCK_NAME"])
+    LockTable.primaryKey = [LockTable.columnNamed("LOCK_NAME")]
+
+    return inSchema
+
+LockSchema = SchemaSyntax(makeLockSchema(Schema(__file__)))
+
+
+
+class Locker(object):
+    """
+    Acquire named locks against a database.
+    """
+
+    def lock(self, name, wait=False):
+        """
+        Acquire a lock with the given name.
+
+        @param name: The name of the lock to acquire.  Against the same store,
+            no two locks may be acquired.
+        @type name: L{unicode}
+
+        @param wait: Whether or not to wait for the lock.  If L{True}, the
+            L{Deferred} returned by L{lock} make some time to fire; if
+            L{False}, it should quickly fail instead.
+
+        @return: a L{Deferred} that fires with an L{AcquiredLock} when the lock
+            has fired, or fails when the lock has not been acquired.
+        """
+        raise NotImplementedError()
+
+
