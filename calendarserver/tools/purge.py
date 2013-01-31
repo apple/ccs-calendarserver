f6c3c04efd509bc6c879e8857b2174030e13e350
Morgen Sagen <sagen@apple.com>
2013-01-31 20:55:10 +0000

    Purge tool should always delete inbox items, not modify them
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/release/CalendarServer-4.3-dev@10608 e27351fd-9f3e-4f54-a53b-843176b1656c

--- calendarserver/tools/purge.py
+++ calendarserver/tools/purge.py
@@ -819,7 +819,8 @@ class PurgePrincipalService(WorkerService):
                         for childName in childNames:
 
                             childResource = (yield collection.getChild(childName))
-                            if self.completely:
+                            # Always delete inbox items
+                            if self.completely or collName == "inbox":
                                 action = self.CANCELEVENT_SHOULD_DELETE
                             else:
                                 event = (yield childResource.iCalendar())
