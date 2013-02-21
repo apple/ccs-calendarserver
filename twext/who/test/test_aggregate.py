f418cf730e9817492da4a88241dd920ccd8ae937
Wilfredo Sanchez <wsanchez@apple.com>
2013-02-21 18:11:19 +0000

    Stub
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@10775 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ twext/who/test/test_aggregate.py
@@ -0,0 +1,38 @@
+##
+# Copyright (c) 2013 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+Aggregate directory service tests
+"""
+
+from twext.who.aggregate import DirectoryService
+
+#from twext.who.test import test_directory
+from twext.who.test.test_xml import xmlService
+
+
+
+class BaseTest(object):
+    def service(self):
+        raise NotImplementedError(DirectoryService)
+
+    def xmlService(self, xmlData=None):
+        return xmlService(self.mktemp(), xmlData)
+
+
+
+#class DirectoryServiceTest(BaseTest, test_directory.DirectoryServiceTest):
+#    pass
