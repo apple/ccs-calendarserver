374e1394d3aed886ecffdd5b6f6b3b2e87de4924
Andre LaBranche <dre@apple.com>
2013-08-12 18:17:19 +0000

    Actually, let's prefer sha1sum over sha1
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@11598 e27351fd-9f3e-4f54-a53b-843176b1656c

--- support/build.sh
+++ support/build.sh
@@ -112,13 +112,13 @@ init_build () {
     md5 () { "$(type -p md5sum)" "$@"; }
   fi;
 
-  if type -ft shasum > /dev/null; then
-    if [ -z "${hash}" ]; then hash="sha1"; fi;
-    sha1 () { "$(type -p shasum)" "$@"; }
-  fi;
   if type -ft sha1sum > /dev/null; then
     if [ -z "${hash}" ]; then hash="sha1sum"; fi;
     sha1 () { "$(type -p sha1sum)" "$@"; }
+  fi;
+  if type -ft shasum > /dev/null; then
+    if [ -z "${hash}" ]; then hash="sha1"; fi;
+    sha1 () { "$(type -p shasum)" "$@"; }
   fi;
 
   if [ "${hash}" == "sha1" ]; then
