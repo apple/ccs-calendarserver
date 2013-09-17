5b82ea845e313fcda9809e7beecb1968b5e76b75
Glyph Lefkowitz <glyph@apple.com>
2013-09-17 23:02:23 +0000

    coding standard: no spaces between {} and dictionary content
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/glyph/log-cleanups@11711 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twext/python/test/test_log.py
+++ twext/python/test/test_log.py
@@ -968,7 +968,7 @@ class LegacyLoggerTests(SetUpTearDown, unittest.TestCase):
         log = TestLegacyLogger()
 
         exception = RuntimeError("Oh me, oh my.")
-        kwargs = { "foo": "bar", "obj": object() }
+        kwargs = {"foo": "bar", "obj": object()}
         why = "Because I said so."
         bogus = object()
 
