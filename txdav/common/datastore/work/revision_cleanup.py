d93b11f0236d534b1250985f9cc315b7c1d30f54
Bruce Gaya <gaya@apple.com>
2014-01-20 22:21:09 +0000

    remove unused import
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@12404 e27351fd-9f3e-4f54-a53b-843176b1656c

--- txdav/common/datastore/work/revision_cleanup.py
+++ txdav/common/datastore/work/revision_cleanup.py
@@ -20,7 +20,7 @@ Remove old and unused REVISION rows
 """
 
 from twext.enterprise.dal.record import fromTable
-from twext.enterprise.dal.syntax import Delete, Select, Update, Max
+from twext.enterprise.dal.syntax import Delete, Select, Max
 from twext.enterprise.queue import WorkItem
 from twext.python.log import Logger
 from twisted.internet.defer import inlineCallbacks, returnValue
