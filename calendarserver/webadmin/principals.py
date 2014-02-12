3be6cb7f5a26c38d2218ae3a21434f02055aabd7
Wilfredo Sanchez <wsanchez@apple.com>
2014-02-12 22:43:45 +0000

    Add record details
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@12684 e27351fd-9f3e-4f54-a53b-843176b1656c

--- calendarserver/webadmin/principals.py
+++ calendarserver/webadmin/principals.py
@@ -45,7 +45,7 @@ class PrincipalsPageElement(PageElement):
 
     def pageSlots(self):
         return {
-            u"title": u"Calendar & Contacts Server Principal Management",
+            u"title": u"Calendar & Contacts Server Principal Search",
         }
 
 
@@ -122,8 +122,42 @@ class PrincipalEditPageElement(PageElement):
 
 
     def pageSlots(self):
+        record = self._record
+
+        def one(value):
+            if value is None:
+                return u"(no value)"
+            else:
+                return unicode(value)
+
+        def many(values):
+            noValues = True
+
+            for value in values:
+                if not noValues:
+                    yield tags.br()
+
+                yield one(value)
+
+                noValues = False
+
+            if noValues:
+                yield u"(no values)"
+
         return {
-            u"title": u"Calendar & Contacts Server Principal Edit",
+            u"title": u"Calendar & Contacts Server Principal Information",
+            u"service": (
+                u"{service.__class__.__name__}: {service.realmName}"
+                .format(service=record.service)
+            ),
+            u"uid": one(record.uid),
+            u"guid": one(record.guid),
+            u"record_type": one(record.recordType),
+            u"short_names": many(record.shortNames),
+            u"full_names": one(record.fullName),
+            u"email_addresses": many(record.emailAddresses),
+            u"calendar_user_addresses": many(record.calendarUserAddresses),
+            u"server_id": one(record.serverID),
         }
 
 
