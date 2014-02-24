8f2611de36af535d225bed1aa248e1b4c95a12d6
Andre LaBranche <dre@apple.com>
2014-02-24 17:56:03 +0000

    Conditional import of OD in unit tests
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@12744 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/directory/test/test_buildquery.py
+++ twistedcaldav/directory/test/test_buildquery.py
@@ -14,143 +14,147 @@
 # limitations under the License.
 ##
 
-from twistedcaldav.test.util import TestCase
-from twistedcaldav.directory.appleopendirectory import (buildQueries,
-    buildLocalQueriesFromTokens, OpenDirectoryService, buildNestedQueryFromTokens)
-from calendarserver.platform.darwin.od import dsattributes
+try:
+    from calendarserver.platform.darwin.od import dsattributes
+except ImportError:
+    pass
+else:
+    from twistedcaldav.test.util import TestCase
+    from twistedcaldav.directory.appleopendirectory import (buildQueries,
+        buildLocalQueriesFromTokens, OpenDirectoryService, buildNestedQueryFromTokens)
 
-class BuildQueryTests(TestCase):
+    class BuildQueryTests(TestCase):
 
-    def test_buildQuery(self):
-        self.assertEquals(
-            buildQueries(
-                [dsattributes.kDSStdRecordTypeUsers],
-                (
-                    ("firstName", "morgen", True, "starts-with"),
-                    ("lastName", "sagen", True, "starts-with"),
+        def test_buildQuery(self):
+            self.assertEquals(
+                buildQueries(
+                    [dsattributes.kDSStdRecordTypeUsers],
+                    (
+                        ("firstName", "morgen", True, "starts-with"),
+                        ("lastName", "sagen", True, "starts-with"),
+                    ),
+                    OpenDirectoryService._ODFields
                 ),
-                OpenDirectoryService._ODFields
-            ),
-            {
-                ('dsAttrTypeStandard:FirstName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers],
-                ('dsAttrTypeStandard:LastName', 'sagen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers],
-            }
-        )
-        self.assertEquals(
-            buildQueries(
-                [
-                    dsattributes.kDSStdRecordTypeUsers,
-                ],
-                (
-                    ("firstName", "morgen", True, "starts-with"),
-                    ("emailAddresses", "morgen", True, "contains"),
+                {
+                    ('dsAttrTypeStandard:FirstName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers],
+                    ('dsAttrTypeStandard:LastName', 'sagen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers],
+                }
+            )
+            self.assertEquals(
+                buildQueries(
+                    [
+                        dsattributes.kDSStdRecordTypeUsers,
+                    ],
+                    (
+                        ("firstName", "morgen", True, "starts-with"),
+                        ("emailAddresses", "morgen", True, "contains"),
+                    ),
+                    OpenDirectoryService._ODFields
                 ),
-                OpenDirectoryService._ODFields
-            ),
-            {
-                ('dsAttrTypeStandard:FirstName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers],
-                ('dsAttrTypeStandard:EMailAddress', 'morgen', True, 'contains') : [dsattributes.kDSStdRecordTypeUsers],
-            }
-        )
-        self.assertEquals(
-            buildQueries(
-                [
-                    dsattributes.kDSStdRecordTypeGroups,
-                ],
-                (
-                    ("firstName", "morgen", True, "starts-with"),
-                    ("lastName", "morgen", True, "starts-with"),
-                    ("fullName", "morgen", True, "starts-with"),
-                    ("emailAddresses", "morgen", True, "contains"),
+                {
+                    ('dsAttrTypeStandard:FirstName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers],
+                    ('dsAttrTypeStandard:EMailAddress', 'morgen', True, 'contains') : [dsattributes.kDSStdRecordTypeUsers],
+                }
+            )
+            self.assertEquals(
+                buildQueries(
+                    [
+                        dsattributes.kDSStdRecordTypeGroups,
+                    ],
+                    (
+                        ("firstName", "morgen", True, "starts-with"),
+                        ("lastName", "morgen", True, "starts-with"),
+                        ("fullName", "morgen", True, "starts-with"),
+                        ("emailAddresses", "morgen", True, "contains"),
+                    ),
+                    OpenDirectoryService._ODFields
                 ),
-                OpenDirectoryService._ODFields
-            ),
-            {
-                ('dsAttrTypeStandard:RealName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeGroups],
-                ('dsAttrTypeStandard:EMailAddress', 'morgen', True, 'contains') : [dsattributes.kDSStdRecordTypeGroups],
-            }
-        )
-        self.assertEquals(
-            buildQueries(
-                [
-                    dsattributes.kDSStdRecordTypeUsers,
-                    dsattributes.kDSStdRecordTypeGroups,
-                ],
-                (
-                    ("firstName", "morgen", True, "starts-with"),
-                    ("lastName", "morgen", True, "starts-with"),
-                    ("fullName", "morgen", True, "starts-with"),
-                    ("emailAddresses", "morgen", True, "contains"),
+                {
+                    ('dsAttrTypeStandard:RealName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeGroups],
+                    ('dsAttrTypeStandard:EMailAddress', 'morgen', True, 'contains') : [dsattributes.kDSStdRecordTypeGroups],
+                }
+            )
+            self.assertEquals(
+                buildQueries(
+                    [
+                        dsattributes.kDSStdRecordTypeUsers,
+                        dsattributes.kDSStdRecordTypeGroups,
+                    ],
+                    (
+                        ("firstName", "morgen", True, "starts-with"),
+                        ("lastName", "morgen", True, "starts-with"),
+                        ("fullName", "morgen", True, "starts-with"),
+                        ("emailAddresses", "morgen", True, "contains"),
+                    ),
+                    OpenDirectoryService._ODFields
                 ),
-                OpenDirectoryService._ODFields
-            ),
-            {
-                ('dsAttrTypeStandard:RealName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers, dsattributes.kDSStdRecordTypeGroups],
-                ('dsAttrTypeStandard:EMailAddress', 'morgen', True, 'contains') : [dsattributes.kDSStdRecordTypeUsers, dsattributes.kDSStdRecordTypeGroups],
-                ('dsAttrTypeStandard:FirstName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers],
-                ('dsAttrTypeStandard:LastName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers],
-            }
-        )
-        self.assertEquals(
-            buildQueries(
-                [
-                    dsattributes.kDSStdRecordTypeGroups,
-                ],
-                (
-                    ("firstName", "morgen", True, "starts-with"),
+                {
+                    ('dsAttrTypeStandard:RealName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers, dsattributes.kDSStdRecordTypeGroups],
+                    ('dsAttrTypeStandard:EMailAddress', 'morgen', True, 'contains') : [dsattributes.kDSStdRecordTypeUsers, dsattributes.kDSStdRecordTypeGroups],
+                    ('dsAttrTypeStandard:FirstName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers],
+                    ('dsAttrTypeStandard:LastName', 'morgen', True, 'starts-with') : [dsattributes.kDSStdRecordTypeUsers],
+                }
+            )
+            self.assertEquals(
+                buildQueries(
+                    [
+                        dsattributes.kDSStdRecordTypeGroups,
+                    ],
+                    (
+                        ("firstName", "morgen", True, "starts-with"),
+                    ),
+                    OpenDirectoryService._ODFields
                 ),
-                OpenDirectoryService._ODFields
-            ),
-            {
-            }
-        )
+                {
+                }
+            )
 
 
-    def test_buildLocalQueryFromTokens(self):
-        """
-        Verify the generating of the simpler queries passed to /Local/Default
-        """
-        results = buildLocalQueriesFromTokens([], OpenDirectoryService._ODFields)
-        self.assertEquals(results, None)
+        def test_buildLocalQueryFromTokens(self):
+            """
+            Verify the generating of the simpler queries passed to /Local/Default
+            """
+            results = buildLocalQueriesFromTokens([], OpenDirectoryService._ODFields)
+            self.assertEquals(results, None)
 
-        results = buildLocalQueriesFromTokens(["foo"], OpenDirectoryService._ODFields)
-        self.assertEquals(
-            results[0].generate(),
-            "(|(dsAttrTypeStandard:RealName=*foo*)(dsAttrTypeStandard:EMailAddress=foo*))"
-        )
+            results = buildLocalQueriesFromTokens(["foo"], OpenDirectoryService._ODFields)
+            self.assertEquals(
+                results[0].generate(),
+                "(|(dsAttrTypeStandard:RealName=*foo*)(dsAttrTypeStandard:EMailAddress=foo*))"
+            )
 
-        results = buildLocalQueriesFromTokens(["foo", "bar"], OpenDirectoryService._ODFields)
-        self.assertEquals(
-            results[0].generate(),
-            "(|(dsAttrTypeStandard:RealName=*foo*)(dsAttrTypeStandard:EMailAddress=foo*))"
-        )
-        self.assertEquals(
-            results[1].generate(),
-            "(|(dsAttrTypeStandard:RealName=*bar*)(dsAttrTypeStandard:EMailAddress=bar*))"
-        )
+            results = buildLocalQueriesFromTokens(["foo", "bar"], OpenDirectoryService._ODFields)
+            self.assertEquals(
+                results[0].generate(),
+                "(|(dsAttrTypeStandard:RealName=*foo*)(dsAttrTypeStandard:EMailAddress=foo*))"
+            )
+            self.assertEquals(
+                results[1].generate(),
+                "(|(dsAttrTypeStandard:RealName=*bar*)(dsAttrTypeStandard:EMailAddress=bar*))"
+            )
 
 
-    def test_buildNestedQueryFromTokens(self):
-        """
-        Verify the generating of the complex nested queries
-        """
-        query = buildNestedQueryFromTokens([], OpenDirectoryService._ODFields)
-        self.assertEquals(query, None)
+        def test_buildNestedQueryFromTokens(self):
+            """
+            Verify the generating of the complex nested queries
+            """
+            query = buildNestedQueryFromTokens([], OpenDirectoryService._ODFields)
+            self.assertEquals(query, None)
 
-        query = buildNestedQueryFromTokens(["foo"], OpenDirectoryService._ODFields)
-        self.assertEquals(
-            query.generate(),
-            "(|(dsAttrTypeStandard:RealName=*foo*)(dsAttrTypeStandard:EMailAddress=foo*)(dsAttrTypeStandard:RecordName=foo*))"
-        )
+            query = buildNestedQueryFromTokens(["foo"], OpenDirectoryService._ODFields)
+            self.assertEquals(
+                query.generate(),
+                "(|(dsAttrTypeStandard:RealName=*foo*)(dsAttrTypeStandard:EMailAddress=foo*)(dsAttrTypeStandard:RecordName=foo*))"
+            )
 
-        query = buildNestedQueryFromTokens(["foo", "bar"], OpenDirectoryService._ODFields)
-        self.assertEquals(
-            query.generate(),
-            "(&(|(dsAttrTypeStandard:RealName=*foo*)(dsAttrTypeStandard:EMailAddress=foo*)(dsAttrTypeStandard:RecordName=foo*))(|(dsAttrTypeStandard:RealName=*bar*)(dsAttrTypeStandard:EMailAddress=bar*)(dsAttrTypeStandard:RecordName=bar*)))"
-        )
+            query = buildNestedQueryFromTokens(["foo", "bar"], OpenDirectoryService._ODFields)
+            self.assertEquals(
+                query.generate(),
+                "(&(|(dsAttrTypeStandard:RealName=*foo*)(dsAttrTypeStandard:EMailAddress=foo*)(dsAttrTypeStandard:RecordName=foo*))(|(dsAttrTypeStandard:RealName=*bar*)(dsAttrTypeStandard:EMailAddress=bar*)(dsAttrTypeStandard:RecordName=bar*)))"
+            )
 
-        query = buildNestedQueryFromTokens(["foo", "bar", "baz"], OpenDirectoryService._ODFields)
-        self.assertEquals(
-            query.generate(),
-            "(&(|(dsAttrTypeStandard:RealName=*foo*)(dsAttrTypeStandard:EMailAddress=foo*)(dsAttrTypeStandard:RecordName=foo*))(|(dsAttrTypeStandard:RealName=*bar*)(dsAttrTypeStandard:EMailAddress=bar*)(dsAttrTypeStandard:RecordName=bar*))(|(dsAttrTypeStandard:RealName=*baz*)(dsAttrTypeStandard:EMailAddress=baz*)(dsAttrTypeStandard:RecordName=baz*)))"
-        )
+            query = buildNestedQueryFromTokens(["foo", "bar", "baz"], OpenDirectoryService._ODFields)
+            self.assertEquals(
+                query.generate(),
+                "(&(|(dsAttrTypeStandard:RealName=*foo*)(dsAttrTypeStandard:EMailAddress=foo*)(dsAttrTypeStandard:RecordName=foo*))(|(dsAttrTypeStandard:RealName=*bar*)(dsAttrTypeStandard:EMailAddress=bar*)(dsAttrTypeStandard:RecordName=bar*))(|(dsAttrTypeStandard:RealName=*baz*)(dsAttrTypeStandard:EMailAddress=baz*)(dsAttrTypeStandard:RecordName=baz*)))"
+            )
