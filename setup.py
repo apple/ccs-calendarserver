017cc423c0552274073ef20f8f16698ab5dc563e
Wilfredo Sanchez <wsanchez@apple.com>
2014-03-18 20:32:58 +0000

    Don't ship push tool
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@12954 e27351fd-9f3e-4f54-a53b-843176b1656c

--- setup.py
+++ setup.py
@@ -246,7 +246,7 @@ def doSetup():
             "bin/py/calendarserver_export",
             # "bin/py/calendarserver_icalendar_validate",
             "bin/py/calendarserver_manage_principals",
-            "bin/py/calendarserver_manage_push",
+            # "bin/py/calendarserver_manage_push",
             # "bin/py/calendarserver_manage_timezones",
             "bin/py/calendarserver_migrate_resources",
             # "bin/py/calendarserver_monitor_amp_notifications",
