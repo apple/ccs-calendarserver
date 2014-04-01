76a4ebaaef13aa57d00103197c95b2863ac7cd22
Wilfredo Sanchez <wsanchez@apple.com>
2014-04-01 02:26:24 +0000

    KABLOOIE!!!
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/sagen/move2who-4@13084 e27351fd-9f3e-4f54-a53b-843176b1656c

--- calendarserver/platform/darwin/od/odframework.py
+++
@@ -1,7 +0,0 @@
-import objc as _objc
-
-__bundle__ = _objc.initFrameworkWrapper("OpenDirectory",
-    frameworkIdentifier="com.apple.OpenDirectory",
-    frameworkPath=_objc.pathForFramework(
-    "/System/Library/Frameworks/OpenDirectory.framework"),
-    globals=globals())
