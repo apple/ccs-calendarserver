9f76c954947551346e62f3640ef56eafbdeb156b
Morgen Sagen <sagen@apple.com>
2014-06-18 20:27:26 +0000

    Removing some hacks because we can now pass recordTypes directly to recordsFromExpression
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/sagen/recordtypes@13653 e27351fd-9f3e-4f54-a53b-843176b1656c

--- txdav/who/directory.py
+++ txdav/who/directory.py
@@ -182,36 +182,16 @@ class CalendarDirectoryServiceMixin(object):
             # CompoundExpression that ANDs the original expression with a
             # typeSpecific one.  Collect all the results from these expressions.
             recordTypes = self.recordTypesForSearchContext(context)
-            log.debug("Tokens: {t}, recordTypes {r}", t=tokens, r=recordTypes)
-            for recordType in recordTypes:
-                typeSpecific = MatchExpression(
-                    self.fieldName.recordType,
-                    recordType,
-                    MatchType.equals,
-                    MatchFlags.none
-                )
-
-                typeSpecific = CompoundExpression(
-                    [expression, typeSpecific],
-                    Operand.AND
-                )
-
-                subResults = yield self.recordsFromExpression(typeSpecific)
-                log.debug(
-                    "Tokens ({t}) matched {n} of {r}",
-                    t=tokens, n=len(subResults), r=recordType
-                )
-                results.extend(subResults)
-
         else:
-            # No record type limits
-            results = yield self.recordsFromExpression(expression)
-            log.debug(
-                "Tokens ({t}) matched {n} records",
-                t=tokens, n=len(results)
-            )
+            recordTypes = None
 
-        log.debug("Tokens ({t}) matched records {r}", t=tokens, r=results)
+        results = yield self.recordsFromExpression(
+            expression, recordTypes=recordTypes
+        )
+        log.debug(
+            "Tokens ({t}) matched {n} records",
+            t=tokens, n=len(results)
+        )
 
         returnValue(results)
 
@@ -254,36 +234,14 @@ class CalendarDirectoryServiceMixin(object):
             )
             subExpressions.append(subExpression)
 
-        if len(subExpressions) == 1:
-            # FIXME: twext.who.opendirectory takes a CompoundExpression and
-            # inspects it for MatchExpressions on recordType, and pulls those
-            # out of the query structure.  However, if all that remains is
-            # essentially a MatchExpression, but we tell OD to use
-            # ODMatchType.compound for it, we get no results back.  Until we
-            # fix twext.who, this workaround keeds the query compound even
-            # when the record type portion is removed:
-            expression = CompoundExpression(
-                [subExpressions[0], subExpressions[0]], operand
-            )
-        else:
-            expression = CompoundExpression(subExpressions, operand)
+        expression = CompoundExpression(subExpressions, operand)
 
-        # AND in the recordType if passed in
         if recordType is not None:
-            typeExpression = MatchExpression(
-                self.fieldName.recordType,
-                recordType,
-                MatchType.equals,
-                MatchFlags.none
-            )
-            expression = CompoundExpression(
-                [
-                    expression,
-                    typeExpression
-                ],
-                Operand.AND
-            )
-        return self.recordsFromExpression(expression)
+            recordTypes = [recordType]
+        else:
+            recordTypes = None
+        return self.recordsFromExpression(expression, recordTypes=recordTypes)
+
 
     _oldRecordTypeNames = {
         "address": "addresses",
