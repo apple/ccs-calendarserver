c14213e632a0b0767cb38261323b367431b995bc
Bruce Gaya <gaya@apple.com>
2014-07-08 09:09:07 +0000

    run txdav/common/datastore/sql_tables.py
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/users/gaya/groupsharee2@13733 e27351fd-9f3e-4f54-a53b-843176b1656c

--- txdav/common/datastore/sql_schema/current-oracle-dialect.sql
+++ txdav/common/datastore/sql_schema/current-oracle-dialect.sql
@@ -116,6 +116,7 @@ insert into CALENDAR_BIND_MODE (DESCRIPTION, ID) values ('read', 1);
 insert into CALENDAR_BIND_MODE (DESCRIPTION, ID) values ('write', 2);
 insert into CALENDAR_BIND_MODE (DESCRIPTION, ID) values ('direct', 3);
 insert into CALENDAR_BIND_MODE (DESCRIPTION, ID) values ('indirect', 4);
+insert into CALENDAR_BIND_MODE (DESCRIPTION, ID) values ('group', 5);
 create table CALENDAR_BIND_STATUS (
     "ID" integer primary key,
     "DESCRIPTION" nvarchar2(16) unique
@@ -402,13 +403,6 @@ create table GROUP_REFRESH_WORK (
     "GROUP_UID" nvarchar2(255)
 );
 
-create table GROUP_ATTENDEE_RECONCILE_WORK (
-    "WORK_ID" integer primary key not null,
-    "JOB_ID" integer not null references JOB,
-    "RESOURCE_ID" integer,
-    "GROUP_ID" integer
-);
-
 create table GROUPS (
     "GROUP_ID" integer primary key,
     "NAME" nvarchar2(255),
@@ -425,11 +419,34 @@ create table GROUP_MEMBERSHIP (
     primary key ("GROUP_ID", "MEMBER_UID")
 );
 
+create table GROUP_ATTENDEE_RECONCILE_WORK (
+    "WORK_ID" integer primary key not null,
+    "JOB_ID" integer not null references JOB,
+    "RESOURCE_ID" integer,
+    "GROUP_ID" integer
+);
+
 create table GROUP_ATTENDEE (
     "GROUP_ID" integer not null references GROUPS on delete cascade,
     "RESOURCE_ID" integer not null references CALENDAR_OBJECT on delete cascade,
     "MEMBERSHIP_HASH" nvarchar2(255), 
     primary key ("GROUP_ID", "RESOURCE_ID")
+);
+
+create table GROUP_SHAREE_RECONCILE_WORK (
+    "WORK_ID" integer primary key not null,
+    "JOB_ID" integer not null references JOB,
+    "CALENDAR_ID" integer not null references CALENDAR on delete cascade,
+    "GROUP_ID" integer not null references GROUPS on delete cascade
+);
+
+create table GROUP_SHAREE (
+    "GROUP_ID" integer not null references GROUPS on delete cascade,
+    "CALENDAR_HOME_ID" integer not null references CALENDAR_HOME on delete cascade,
+    "CALENDAR_ID" integer not null references CALENDAR on delete cascade,
+    "GROUP_BIND_MODE" integer not null,
+    "MEMBERSHIP_HASH" nvarchar2(255), 
+    primary key ("GROUP_ID", "CALENDAR_HOME_ID", "CALENDAR_ID")
 );
 
 create table DELEGATES (
@@ -736,10 +753,6 @@ create index GROUP_REFRESH_WORK_JO_717ede20 on GROUP_REFRESH_WORK (
     JOB_ID
 );
 
-create index GROUP_ATTENDEE_RECONC_da73d3c2 on GROUP_ATTENDEE_RECONCILE_WORK (
-    JOB_ID
-);
-
 create index GROUPS_GROUP_UID_b35cce23 on GROUPS (
     GROUP_UID
 );
@@ -748,8 +761,20 @@ create index GROUP_MEMBERSHIP_MEMB_0ca508e8 on GROUP_MEMBERSHIP (
     MEMBER_UID
 );
 
-create index GROUP_ATTENDEE_RESOUR_855124dc on GROUP_ATTENDEE (
+create index GROUP_ATTENDEE_RECONC_da73d3c2 on GROUP_ATTENDEE_RECONCILE_WORK (
+    JOB_ID
+);
+
+create index GROUP_ATTENDEE_ID_d497ffdb on GROUP_ATTENDEE (
     RESOURCE_ID
+);
+
+create index GROUP_SHAREE_RECONCIL_9aad0858 on GROUP_SHAREE_RECONCILE_WORK (
+    JOB_ID
+);
+
+create index GROUP_SHAREE_CALENDAR_28a88850 on GROUP_SHAREE (
+    CALENDAR_ID
 );
 
 create index DELEGATE_TO_DELEGATOR_5e149b11 on DELEGATES (
