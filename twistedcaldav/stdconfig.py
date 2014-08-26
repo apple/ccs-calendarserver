8021882093205ab67d61b2e6c1db4dd41ea5a52e
Cyrus Daboo <cdaboo@apple.com>
2014-08-26 20:03:59 +0000

    Enhance TLS security.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@13919 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/stdconfig.py
+++ twistedcaldav/stdconfig.py
@@ -155,8 +155,8 @@ DEFAULT_CONFIG = {
     "SSLPort": 0, # SSL port (0 to disable HTTPS)
     "EnableSSL": False, # Whether to listen on SSL port(s)
     "RedirectHTTPToHTTPS": False, # If True, all nonSSL requests redirected to an SSL Port
-    "SSLMethod": "SSLv3_METHOD", # SSLv2_METHOD, SSLv3_METHOD, SSLv23_METHOD, TLSv1_METHOD
-    "SSLCiphers": "ALL:!aNULL:!ADH:!eNULL:!LOW:!EXP:RC4+RSA:+HIGH:+MEDIUM",
+    "SSLMethod": "SSLv23_METHOD", # SSLv2_METHOD, SSLv3_METHOD, SSLv23_METHOD, TLSv1_METHOD
+    "SSLCiphers": "RC4-SHA:HIGH:!ADH",
 
     # Max-age value for Strict-Transport-Security header; set to 0 to
     # disable header.
