bd008d47f6d7f10fd952b10e59aa5d4bb4385925
Cyrus Daboo <cdaboo@apple.com>
2014-09-16 02:00:47 +0000

    Additional indexes for performance. Hash partitioning upgrade/downgrade scripts.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/release/CalendarServer-5.3-dev@13966 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ txdav/common/datastore/sql_schema/manual_upgrades/manual_upgrade_3.sql
@@ -0,0 +1,56 @@
+----
+-- Copyright (c) 2012-2014 Apple Inc. All rights reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+----
+
+---------------------------------------------------
+-- Upgrade database schema from VERSION XX to YY --
+---------------------------------------------------
+
+
+-- Update to hash partitioned indexes
+
+-- CALENDAR_OBJECT Table
+
+-- Disable the pkey and foreign key constraints
+ALTER TABLE CALENDAR_OBJECT DISABLE CONSTRAINT SYS_C004279 CASCADE;
+
+-- Hash partition the primary key index
+ALTER TABLE CALENDAR_OBJECT ENABLE CONSTRAINT SYS_C004279 USING INDEX TABLESPACE DATA_TS GLOBAL PARTITION BY HASH (RESOURCE_ID) PARTITIONS 32;
+
+-- Enable the foreign key constraints
+ALTER TABLE TIME_RANGE ENABLE CONSTRAINT SYS_C004296;
+ALTER TABLE ATTACHMENT_CALENDAR_OBJECT ENABLE CONSTRAINT SYS_C0065636;
+ALTER TABLE CALENDAR_OBJECT_SPLITTER_WORK ENABLE CONSTRAINT SYS_C0089711;
+
+
+-- TIME_RANGE Table
+
+-- Disable the pkey and foreign key constraints
+ALTER TABLE TIME_RANGE DISABLE CONSTRAINT SYS_C004294 DISABLE CASCADE;
+
+-- Hash partition the primary key index
+ALTER TABLE TIME_RANGE ENABLE CONSTRAINT SYS_C004294 USING INDEX TABLESPACE DATA_TS GLOBAL PARTITION BY HASH (INSTANCE_ID) PARTITIONS 32;
+
+-- Enable the foreign key constraints
+ALTER TABLE TRANSPARENCY ENABLE CONSTRAINT SYS_C004301;
+
+
+-- PUSH_NOTIFICATION_WORK Table
+
+-- Disable the pkey and foreign key constraints
+ALTER TABLE PUSH_NOTIFICATION_WORK DISABLE CONSTRAINT SYS_C0013546 CASCADE;
+
+-- Hash partition the primary key index
+ALTER TABLE PUSH_NOTIFICATION_WORK ENABLE CONSTRAINT SYS_C0013546 USING INDEX TABLESPACE DATA_TS GLOBAL PARTITION BY HASH (WORK_ID) PARTITIONS 32;
