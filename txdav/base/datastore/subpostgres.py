8d2535dcf665818047bb3408030530cb9afe2bfe
Morgen Sagen <sagen@apple.com>
2014-10-06 17:08:09 +0000

    Tell pg_ctl to wait longer to account for a long cluster upgrade
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/release/CalendarServer-5.2.1-dev@14043 e27351fd-9f3e-4f54-a53b-843176b1656c

--- txdav/base/datastore/subpostgres.py
+++ txdav/base/datastore/subpostgres.py
@@ -228,7 +228,7 @@ class PostgresService(MultiService):
                 # in /tmp and based on a hash of the data store directory
                 digest = md5(dataStoreDirectory.path).hexdigest()
                 socketDir = "/tmp/ccs_postgres_" + digest
-                
+
             self.socketDir = CachingFilePath(socketDir)
             self.host = self.socketDir.path
             self.port = None
@@ -425,6 +425,7 @@ class PostgresService(MultiService):
                 pgCtl,
                 "start",
                 "-l", self.logFile,
+                "-t 86400",  # Give plenty of time for a long cluster upgrade
                 "-w",
                 # XXX what are the quoting rules for '-o'?  do I need to repr()
                 # the path here?
