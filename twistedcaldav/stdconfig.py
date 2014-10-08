c6a3ad7e54614a8020bb07a785578947980ea821
Wilfredo Sanchez <wsanchez@apple.com>
2014-10-08 23:03:05 +0000

    Enable SSL v2/v3 negotiation
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/branches/release/CalendarServer-4.2.2-dev@14064 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/stdconfig.py
+++ twistedcaldav/stdconfig.py
@@ -248,7 +248,7 @@ DEFAULT_CONFIG = {
     "SSLPort" : 0,                 # SSL port (0 to disable HTTPS)
     "EnableSSL" : False,           # Whether to listen on SSL port(s)
     "RedirectHTTPToHTTPS" : False, # If True, all nonSSL requests redirected to an SSL Port
-    "SSLMethod" : "SSLv3_METHOD",  # SSLv2_METHOD, SSLv3_METHOD, SSLv23_METHOD, TLSv1_METHOD
+    "SSLMethod" : "SSLv23_METHOD",  # SSLv2_METHOD, SSLv3_METHOD, SSLv23_METHOD, TLSv1_METHOD
     "SSLCiphers" : "ALL:!aNULL:!ADH:!eNULL:!LOW:!EXP:RC4+RSA:+HIGH:+MEDIUM",
 
     #
