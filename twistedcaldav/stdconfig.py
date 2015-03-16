558f600764747c15c502ae51996a79e5c47f82d2
Morgen Sagen <sagen@apple.com>
2015-03-16 20:03:45 +0000

    The 'log all sql statements' flag was probably enabled by mistake
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@14600 e27351fd-9f3e-4f54-a53b-843176b1656c

--- twistedcaldav/stdconfig.py
+++ twistedcaldav/stdconfig.py
@@ -421,7 +421,7 @@ DEFAULT_CONFIG = {
         "LabelsInSQL"            : False,
         "Statistics"             : False,
         "StatisticsLogFile"      : "sqlstats.log",
-        "SQLStatements"          : True,
+        "SQLStatements"          : False,
         "TransactionWaitSeconds" : 0,
     },
 
