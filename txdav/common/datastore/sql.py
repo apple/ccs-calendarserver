848d2445de379d76a816561c9ebebae681c33139
Cyrus Daboo <cdaboo@apple.com>
2015-04-30 18:55:14 +0000

    Fix per-delegate properties.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@14725 e27351fd-9f3e-4f54-a53b-843176b1656c

--- txdav/common/datastore/sql.py
+++ txdav/common/datastore/sql.py
@@ -3286,7 +3286,7 @@ class CommonHomeChild(FancyEqMixin, Memoizable, _SharedSyncLogic, HomeChildBase,
             childResourceIDs = [dataRow[resourceID_index] for dataRow in dataRows]
 
             propertyStores = yield PropertyStore.forMultipleResourcesWithResourceIDs(
-                home.uid(), None, None, home._txn, childResourceIDs
+                home.uid(), None, home.authzuid(), home._txn, childResourceIDs
             )
 
             # Get revisions
@@ -4577,9 +4577,9 @@ class CommonObjectResource(FancyEqMixin, object):
             # Get property stores for all these child resources (if any found)
             if parent.objectResourcesHaveProperties():
                 propertyStores = (yield PropertyStore.forMultipleResources(
-                    parent._home.uid(),
-                    None,
-                    None,
+                    parent.ownerHome().uid(),
+                    parent.viewerHome().uid(),
+                    parent._home.authzuid(),
                     parent._txn,
                     cls._objectSchema.RESOURCE_ID,
                     cls._objectSchema.PARENT_RESOURCE_ID,
@@ -4683,9 +4683,9 @@ class CommonObjectResource(FancyEqMixin, object):
             # Get property stores for all these child resources
             if parent.objectResourcesHaveProperties():
                 propertyStores = (yield PropertyStore.forMultipleResourcesWithResourceIDs(
-                    parent._home.uid(),
-                    None,
-                    None,
+                    parent.ownerHome().uid(),
+                    parent.viewerHome().uid(),
+                    parent.ownerHome().authzuid(),
                     parent._txn,
                     tuple([row[0] for row in dataRows]),
                 ))
