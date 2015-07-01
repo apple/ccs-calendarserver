f2a6d67f7260debd4861bcc2fb3b35a15593a905
Cyrus Daboo <cdaboo@apple.com>
2015-07-01 14:21:26 +0000

    Include a test for cross-pod attachment size.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@14936 e27351fd-9f3e-4f54-a53b-843176b1656c

--- txdav/common/datastore/podding/test/test_conduit.py
+++ txdav/common/datastore/podding/test/test_conduit.py
@@ -970,10 +970,12 @@ END:VCALENDAR
         yield self.commitTransaction(0)
 
         shared_object = yield self.calendarObjectUnderTest(txn=self.theTransactionUnderTest(1), home="puser01", calendar_name="shared-calendar", name="1.ics")
-        attachment, location = yield shared_object.addAttachment(None, MimeType.fromString("text/plain"), "test.txt", MemoryStream("Here is some text."))
+        data = "Here is some text."
+        attachment, location = yield shared_object.addAttachment(None, MimeType.fromString("text/plain"), "test.txt", MemoryStream(data))
         managedID = attachment.managedID()
         from txdav.caldav.datastore.sql_external import ManagedAttachmentExternal
         self.assertTrue(isinstance(attachment, ManagedAttachmentExternal))
+        self.assertEqual(attachment.size(), len(data))
         self.assertTrue("user01/attachments/test" in location)
         yield self.commitTransaction(1)
 
@@ -1005,10 +1007,12 @@ END:VCALENDAR
         yield self.commitTransaction(0)
 
         shared_object = yield self.calendarObjectUnderTest(txn=self.theTransactionUnderTest(1), home="puser01", calendar_name="shared-calendar", name="1.ics")
-        attachment, location = yield shared_object.updateAttachment(managedID, MimeType.fromString("text/plain"), "test.txt", MemoryStream("Here is some more text."))
+        data = "Here is some more text."
+        attachment, location = yield shared_object.updateAttachment(managedID, MimeType.fromString("text/plain"), "test.txt", MemoryStream(data))
         managedID = attachment.managedID()
         from txdav.caldav.datastore.sql_external import ManagedAttachmentExternal
         self.assertTrue(isinstance(attachment, ManagedAttachmentExternal))
+        self.assertEqual(attachment.size(), len(data))
         self.assertTrue("user01/attachments/test" in location)
         yield self.commitTransaction(1)
 
