73b6f9b302434b79bbcdef69a913f507975a7d55
Cyrus Daboo <cdaboo@apple.com>
2016-05-20 19:01:26 +0000

    Add index on REVISION columns to improve performance of revision clean up queries.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalendarServer/trunk@15625 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ txdav/common/datastore/sql_schema/upgrades/postgres-dialect/upgrade_from_60_to_61.sql
@@ -0,0 +1,32 @@
+----
+-- Copyright (c) 2012-2016 Apple Inc. All rights reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+----
+
+---------------------------------------------------
+-- Upgrade database schema from VERSION 60 to 61 --
+---------------------------------------------------
+
+-- Create indexes
+create index CALENDAR_OBJECT_REVISIONS_REVISION
+  on CALENDAR_OBJECT_REVISIONS(REVISION);
+
+create index ADDRESSBOOK_OBJECT_REVISIONS_REVISION
+  on ADDRESSBOOK_OBJECT_REVISIONS(REVISION);
+
+create index NOTIFICATION_OBJECT_REVISIONS_REVISION
+  on NOTIFICATION_OBJECT_REVISIONS(REVISION);
+
+-- update the version
+update CALENDARSERVER set VALUE = '61' where NAME = 'VERSION';
