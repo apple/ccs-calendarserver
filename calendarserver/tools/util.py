b304ab104e168391e92646a9e038b501e78cd5e0
Cyrus Daboo <cdaboo@apple.com>
2016-11-03 10:48:42 -0400

    Config key may not be present.

--- calendarserver/tools/util.py
+++ calendarserver/tools/util.py
@@ -95,7 +95,7 @@ def autoDisableMemcached(config):
     for pool in config.Memcached.Pools.itervalues():
         if pool.ClientEnabled:
             try:
-                if pool.MemcacheSocket:
+                if pool.get("MemcacheSocket"):
                     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
                     s.connect(pool.MemcacheSocket)
                     s.close()
